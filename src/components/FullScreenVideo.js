8dab3413913071dc73442d5042bdc0ec44987299
Ajay Bhatia <prof.ajaybhatia@gmail.com>
2018-08-15 19:41:57 +0530

    Project setup

---
+++ src/components/FullScreenVideo.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, TouchableHighlight, View, ListView, Image } from 'react-native';
+import { RTCView } from 'react-native-webrtc';
+import config from '../config/app.js';
+import styles from '../../style/fullScreenVideo.js';
+
+export default class FullScreenVideo extends Component {
+  render() {
+    return <View style={styles.container}>
+      {
+        config.useRCTView ?
+          <RTCView streamURL={this.props.streamURL} style={styles.video} /> :
+          <Image source={this.props.streamURL} style={styles.video} resizeMode={'contain'} />
+      }
+    </View>
+  }
+}
