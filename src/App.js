8b74f93ca8a468321e987d9036aefac96503d25f
ajaybhatia <prof.ajaybhatia@gmail.com>
2019-02-23 15:59:41 +0530

    Update according to newerversion of react-native-webrtc

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import {
   StyleSheet,
   Text,
@@ -7,85 +7,109 @@ import {
   ListView,
   Image,
   TextInput
-} from 'react-native';
-import { RTCView } from 'react-native-webrtc';
-import Thumbnails from './components/Thumbnails';
-import FullScreenVideo from './components/FullScreenVideo';
-import Commons from './lib/commons';
-import styles from '../style/app';
-import config from './config/app';
+} from "react-native";
+import { RTCView } from "react-native-webrtc";
+import Thumbnails from "./components/Thumbnails";
+import FullScreenVideo from "./components/FullScreenVideo";
+import Commons from "./lib/commons";
+import styles from "../style/app";
+import config from "./config/app";
 
 const sampleStreamURLs = [
-  require('../image/sample-image-1.jpg'),
-  require('../image/sample-image-2.jpg'),
-  require('../image/sample-image-3.jpg')
-]
+  require("../image/sample-image-1.jpg"),
+  require("../image/sample-image-2.jpg"),
+  require("../image/sample-image-3.jpg")
+];
 
-const sampleFullScreenURL = require('../image/sample-image-2.jpg');
-const backgroundImage = require('../image/IMG_0187.jpg');
+const sampleFullScreenURL = require("../image/sample-image-2.jpg");
+const backgroundImage = require("../image/IMG_0187.jpg");
 
 const FRONT_CAMERA = true;
-const webRTCServices = require('./lib/services');
-const VIDEO_CONFERENCE_ROOM = 'video_conference';
+const webRTCServices = require("./lib/services");
+const VIDEO_CONFERENCE_ROOM = "video_conference";
 
-const SELF_STREAM_ID = 'self_stream_id';
+const SELF_STREAM_ID = "self_stream_id";
 
 export default class App extends Component {
   state = {
     activeStreamId: null,
     //streamURLs: sampleStreamURLs,
     streams: [], //list of (id, url: friend Stream URL). Id = socketId
-    joinState: 'ready', //joining, joined
-    name: ''
-  }
+    joinState: "ready", //joining, joined
+    name: ""
+  };
 
   componentDidMount() {
-    webRTCServices.getLocalStream(true, (stream) => {
+    webRTCServices.getLocalStream(true, stream => {
+      console.log("My Stream:", stream.toURL());
       this.setState({
         activeStreamId: SELF_STREAM_ID,
-        streams: [{
-          id: SELF_STREAM_ID,
-          url: stream.toURL()
-        }]
-      })
+        streams: [
+          {
+            id: SELF_STREAM_ID,
+            url: stream.toURL()
+          }
+        ]
+      });
     });
   }
 
   render() {
-    let activeStreamResult = this.state.streams.filter(stream => stream.id == this.state.activeStreamId);
+    let activeStreamResult = this.state.streams.filter(
+      stream => stream.id == this.state.activeStreamId
+    );
 
-    return <View style={styles.container}>
-      <Image source={backgroundImage} blurRadius={3} style={styles.backgroundImage} />
-      <View style={styles.backgroundOverlay} />
-      {
-        this.state.joinState == 'joined' ?
-          <FullScreenVideo streamURL={activeStreamResult.length > 0 ? activeStreamResult[0].url : null} /> :
-          null
-      }
-      {
-        this.state.joinState == 'joined' ?
-          <Thumbnails streams={this.state.streams}
+    return (
+      <View style={styles.container}>
+        <Image
+          source={backgroundImage}
+          blurRadius={3}
+          style={styles.backgroundImage}
+        />
+        <View style={styles.backgroundOverlay} />
+        {this.state.joinState == "joined" ? (
+          <FullScreenVideo
+            streamURL={
+              activeStreamResult.length > 0 ? activeStreamResult[0].url : null
+            }
+          />
+        ) : null}
+        {this.state.joinState == "joined" ? (
+          <Thumbnails
+            streams={this.state.streams}
             setActive={this.handleSetActive.bind(this)}
-            activeStreamId={this.state.activeStreamId} /> :
-          null
-      }
-      {this.renderJoinContainer()}
-    </View>
+            activeStreamId={this.state.activeStreamId}
+          />
+        ) : null}
+        {this.renderJoinContainer()}
+      </View>
+    );
   }
 
   renderJoinContainer() {
-    if (this.state.joinState != 'joined') {
-      return <View style={styles.joinContainer}>
-        <Text style={styles.joinLabel}>Be the first to join this conversation</Text>
-        <TextInput style={styles.joinName}
-          placeholder={'Enter your name'} placeholderTextColor={'#888'}
-          onChangeText={(name) => this.setState({ name })}
-          value={this.state.name} />
-        <TouchableHighlight style={styles.joinButton}
-          onPress={this.handleJoinClick.bind(this)}>
-          <Text style={styles.joinButtonText}>{this.state.joinState == 'ready' ? 'Join' : 'Joining...'}</Text>
-        </TouchableHighlight>
-      </View>
+    if (this.state.joinState != "joined") {
+      return (
+        <View style={styles.joinContainer}>
+          <Text style={styles.joinLabel}>
+            Be the first to join this conversation
+          </Text>
+          <TextInput
+            style={styles.joinName}
+            placeholder={"Enter your name"}
+            placeholderTextColor={"#888"}
+            onChangeText={name => this.setState({ name })}
+            value={this.state.name}
+          />
+          <TouchableHighlight
+            style={styles.joinButton}
+            onPress={this.handleJoinClick.bind(this)}
+          >
+            <Text style={styles.joinButtonText}>
+              {this.state.joinState == "ready" ? "Join" : "Joining..."}
+            </Text>
+          </TouchableHighlight>
+        </View>
+      );
     }
     return null;
   }
@@ -97,35 +121,35 @@ export default class App extends Component {
   }
 
   handleJoinClick() {
-    if (this.state.name.length == 0 || this.state.joinState != 'ready') {
+    if (this.state.name.length == 0 || this.state.joinState != "ready") {
       return;
     }
     //ELSE:
     this.setState({
-      joinState: 'joining'
+      joinState: "joining"
     });
     let callbacks = {
       joined: this.handleJoined.bind(this),
       friendConnected: this.handleFriendConnected.bind(this),
       friendLeft: this.handleFriendLeft.bind(this),
       dataChannelMessage: this.handleDataChannelMessage.bind(this)
-    }
+    };
     webRTCServices.join(VIDEO_CONFERENCE_ROOM, this.state.name, callbacks);
   }
 
   //----------------------------------------------------------------------------
   //  WebRTC service callbacks
   handleJoined() {
-    console.log('Joined');
+    console.log("Joined");
     this.setState({
-      joinState: 'joined'
+      joinState: "joined"
     });
   }
 
   handleFriendLeft(socketId) {
     let newState = {
       streams: this.state.streams.filter(stream => stream.id != socketId)
-    }
+    };
     if (this.state.activeStreamId == socketId) {
       newState.activeStreamId = newState.streams[0].id;
     }
@@ -141,10 +165,8 @@ export default class App extends Component {
           url: stream.toURL()
         }
       ]
-    })
+    });
   }
 
-  handleDataChannelMessage(message) {
-
-  }
+  handleDataChannelMessage(message) {}
 }
--- src/lib/services.js
+++ src/lib/services.js
@@ -1,32 +1,31 @@
-const IS_BROWSER = (module == null || module.exports == null);
+import {
+  RTCPeerConnection,
+  RTCIceCandidate,
+  RTCSessionDescription,
+  RTCView,
+  MediaStream,
+  MediaStreamTrack,
+  mediaDevices
+} from "react-native-webrtc";
 
-let WebRTC = null;
-
-if(IS_BROWSER) {
-  WebRTC = {
-    MediaStreamTrack: window.MediaStreamTrack,
-    getUserMedia: window.navigator.getUserMedia,
-    RTCPeerConnection: window.RTCPeerConnection,
-    RTCSessionDescription: window.RTCSessionDescription,
-    RTCIceCandidate: window.RTCIceCandidate
-  }
-} else {
-  WebRTC = require('react-native-webrtc');
-}
+const IS_BROWSER = module == null || module.exports == null;
 
 let socket = null;
 let onFriendLeftCallback = null;
 let onFriendConnectedCallback = null;
 let onDataChannelMessageCallback = null;
 
-if(IS_BROWSER) {
+if (IS_BROWSER) {
   socket = io();
 } else {
-  const socketIOClient = require('socket.io-client');
-  socket = socketIOClient('https://webrtc-serve.herokuapp.com', {transports: ['websocket'], jsonp: false});
+  const socketIOClient = require("socket.io-client");
+  socket = socketIOClient("https://webrtc-serve.herokuapp.com", {
+    transports: ["websocket"],
+    jsonp: false
+  });
 }
 
-var configuration = {"iceServers": [{"url": "stun:stun.l.google.com:19302"}]};
+var configuration = { iceServers: [{ url: "stun:stun.l.google.com:19302" }] };
 var peerConnections = {}; //map of {socketId: socket.io id, RTCPeerConnection}
 let localStream = null;
 let friends = null; //list of {socketId, name}
@@ -34,53 +33,56 @@ let me = null; //{socketId, name}
 
 function createPeerConnection(friend, isOffer, onDataChannelMessage) {
   let socketId = friend.socketId;
-  var retVal = new WebRTC.RTCPeerConnection(configuration);
+  var retVal = new RTCPeerConnection(configuration);
 
   peerConnections[socketId] = retVal;
 
-  retVal.onicecandidate = function (event) {
-    console.log('onicecandidate');
+  retVal.onicecandidate = function(event) {
+    console.log("onicecandidate");
     if (event.candidate) {
-      socket.emit('exchange', {'to': socketId, 'candidate': event.candidate});
+      socket.emit("exchange", { to: socketId, candidate: event.candidate });
     }
   };
 
   function createOffer() {
-    retVal.createOffer(function(desc) {
-      console.log('createOffer', desc);
-      retVal.setLocalDescription(desc, function () {
-        console.log('setLocalDescription', retVal.localDescription);
-        socket.emit('exchange', {'to': socketId, 'sdp': retVal.localDescription });
-      }, logError);
-    }, logError);
+    retVal.createOffer().then(desc => {
+      retVal.setLocalDescription(desc).then(() => {
+        // Send pc.localDescription to peer
+        console.log("createOffer", desc);
+        socket.emit("exchange", {
+          to: socketId,
+          sdp: retVal.localDescription
+        });
+      });
+    });
   }
 
-  retVal.onnegotiationneeded = function () {
-    console.log('onnegotiationneeded');
+  retVal.onnegotiationneeded = function() {
+    console.log("onnegotiationneeded");
     if (isOffer) {
       createOffer();
     }
-  }
+  };
 
   retVal.oniceconnectionstatechange = function(event) {
-    console.log('oniceconnectionstatechange');
-    if (event.target.iceConnectionState === 'connected') {
+    console.log("oniceconnectionstatechange");
+    if (event.target.iceConnectionState === "connected") {
       createDataChannel();
     }
   };
 
   retVal.onsignalingstatechange = function(event) {
-    console.log('onsignalingstatechange');
+    console.log("onsignalingstatechange");
   };
 
-  retVal.onaddstream = function (event) {
-    console.log('onaddstream');
+  retVal.onaddstream = function(event) {
+    console.log("onaddstream");
     //var element = document.createElement('video');
     //element.id = "remoteView" + socketId;
     //element.autoplay = 'autoplay';
     //element.src = URL.createObjectURL(event.stream);
     //remoteViewContainer.appendChild(element);
-    if(onFriendConnectedCallback != null) {
+    if (onFriendConnectedCallback != null) {
       onFriendConnectedCallback(socketId, event.stream);
     }
   };
@@ -93,22 +95,22 @@ function createPeerConnection(friend, isOffer, onDataChannelMessage) {
     }
     var dataChannel = retVal.createDataChannel("text");
 
-    dataChannel.onerror = function (error) {
+    dataChannel.onerror = function(error) {
       console.log("dataChannel.onerror", error);
     };
 
-    dataChannel.onmessage = function (event) {
+    dataChannel.onmessage = function(event) {
       console.log("dataChannel.onmessage:", event.data);
-      if(onDataChannelMessageCallback != null) {
+      if (onDataChannelMessageCallback != null) {
         onDataChannelMessageCallback(JSON.parse(event.data));
       }
     };
 
-    dataChannel.onopen = function () {
-      console.log('dataChannel.onopen');
+    dataChannel.onopen = function() {
+      console.log("dataChannel.onopen");
     };
 
-    dataChannel.onclose = function () {
+    dataChannel.onclose = function() {
       console.log("dataChannel.onclose");
     };
 
@@ -124,54 +126,65 @@ function exchange(data) {
   if (fromId in peerConnections) {
     pc = peerConnections[fromId];
   } else {
-    let friend = friends.filter((friend) => friend.socketId == fromId)[0];
-    if(friend == null) {
+    let friend = friends.filter(friend => friend.socketId == fromId)[0];
+    if (friend == null) {
       friend = {
         socketId: fromId,
         name: ""
-      }
+      };
     }
     pc = createPeerConnection(friend, false);
   }
 
   if (data.sdp) {
     //console.log('exchange sdp', data);
-    pc.setRemoteDescription(new WebRTC.RTCSessionDescription(data.sdp), function () {
-      if (pc.remoteDescription.type == "offer")
-      pc.createAnswer(function(desc) {
-        //console.log('createAnswer', desc);
-        pc.setLocalDescription(desc, function () {
-          //console.log('setLocalDescription', pc.localDescription);
-          socket.emit('exchange', {'to': fromId, 'sdp': pc.localDescription });
-        }, logError);
-      }, logError);
-    }, logError);
+    pc.setRemoteDescription(
+      new RTCSessionDescription(data.sdp),
+      function() {
+        if (pc.remoteDescription.type == "offer")
+          pc.createAnswer(function(desc) {
+            //console.log('createAnswer', desc);
+            pc.setLocalDescription(
+              desc,
+              function() {
+                //console.log('setLocalDescription', pc.localDescription);
+                socket.emit("exchange", {
+                  to: fromId,
+                  sdp: pc.localDescription
+                });
+              },
+              logError
+            );
+          }, logError);
+      },
+      logError
+    );
   } else {
     //console.log('exchange candidate', data);
-    pc.addIceCandidate(new WebRTC.RTCIceCandidate(data.candidate));
+    pc.addIceCandidate(new RTCIceCandidate(data.candidate));
   }
 }
 
 function leave(socketId) {
-  console.log('leave', socketId);
+  console.log("leave", socketId);
   var pc = peerConnections[socketId];
   pc.close();
   delete peerConnections[socketId];
-  if(onFriendLeftCallback != null) {
+  if (onFriendLeftCallback != null) {
     onFriendLeftCallback(socketId);
   }
 }
 
-socket.on('exchange', function(data){
+socket.on("exchange", function(data) {
   exchange(data);
 });
 
-socket.on('leave', function(socketId){
+socket.on("leave", function(socketId) {
   leave(socketId);
 });
 
-socket.on('connect', function(data) {
-  console.log('connect');
+socket.on("connect", function(data) {
+  console.log("connect");
 });
 
 socket.on("join", function(friend) {
@@ -190,49 +203,88 @@ function logError(error) {
 //------------------------------------------------------------------------------
 // Services
 function countFriends(roomId, callback) {
-  socket.emit("count", roomId, (count) => {
+  socket.emit("count", roomId, count => {
     console.log("Count friends result: ", count);
     callback(count);
   });
 }
 
 function loadLocalStream2(muted) {
-  navigator.getUserMedia({ "audio": true, "video": true }, function (stream) {
-    localStream = stream;
-    var selfView = document.getElementById("selfView");
-    selfView.src = URL.createObjectURL(stream);
-    selfView.muted = muted;
-  }, logError);
+  navigator.getUserMedia(
+    { audio: true, video: true },
+    function(stream) {
+      localStream = stream;
+      var selfView = document.getElementById("selfView");
+      selfView.src = URL.createObjectURL(stream);
+      selfView.muted = muted;
+    },
+    logError
+  );
 }
 
 function getLocalStream(isFront, callback) {
-  WebRTC.MediaStreamTrack.getSources(sourceInfos => {
+  // MediaStreamTrack.getSources(sourceInfos => {
+  //   console.log(sourceInfos);
+  //   let videoSourceId;
+  //   for (const i = 0; i < sourceInfos.length; i++) {
+  //     const sourceInfo = sourceInfos[i];
+  //     if(sourceInfo.kind == "video" && sourceInfo.facing == (isFront ? "front" : "back")) {
+  //       videoSourceId = sourceInfo.id;
+  //     }
+  //   }
+  //   getUserMedia({
+  //     audio: true,
+  //     video: {
+  //       mandatory: {
+  //         minWidth: 500, // Provide your own width, height and frame rate here
+  //         minHeight: 300,
+  //         minFrameRate: 30
+  //       },
+  //       facingMode: (isFront ? "user" : "environment"),
+  //       optional: (videoSourceId ? [{sourceId: videoSourceId}] : [])
+  //     }
+  //   }, function (stream) {
+  //     localStream = stream;
+  //     console.log("Got Local Stream");
+  //     callback(stream);
+  //   }, (error) => {
+  //     console.log("Get LocalStream Fail: ", error);
+  //   });
+  // });
+  mediaDevices.enumerateDevices().then(sourceInfos => {
     console.log(sourceInfos);
     let videoSourceId;
-    for (const i = 0; i < sourceInfos.length; i++) {
+    for (let i = 0; i < sourceInfos.length; i++) {
       const sourceInfo = sourceInfos[i];
-      if(sourceInfo.kind == "video" && sourceInfo.facing == (isFront ? "front" : "back")) {
+      if (
+        sourceInfo.kind == "video" &&
+        sourceInfo.facing == (isFront ? "front" : "back")
+      ) {
         videoSourceId = sourceInfo.id;
       }
     }
-    WebRTC.getUserMedia({
-      audio: true,
-      video: {
-        mandatory: {
-          minWidth: 500, // Provide your own width, height and frame rate here
-          minHeight: 300,
-          minFrameRate: 30
-        },
-        facingMode: (isFront ? "user" : "environment"),
-        optional: (videoSourceId ? [{sourceId: videoSourceId}] : [])
-      }
-    }, function (stream) {
-      localStream = stream;
-      console.log("Got Local Stream");
-      callback(stream);
-    }, (error) => {
-      console.log("Get LocalStream Fail: ", error);
-    });
+    mediaDevices
+      .getUserMedia({
+        audio: true,
+        video: {
+          mandatory: {
+            minWidth: 500, // Provide your own width, height and frame rate here
+            minHeight: 300,
+            minFrameRate: 30
+          },
+          facingMode: isFront ? "user" : "environment",
+          optional: videoSourceId ? [{ sourceId: videoSourceId }] : []
+        }
+      })
+      .then(stream => {
+        // Got stream!
+        localStream = stream;
+        console.log("Got Local Stream");
+        callback(stream);
+      })
+      .catch(error => {
+        // Log error
+      });
   });
 }
 
@@ -257,28 +309,28 @@ function join(roomId, name, callbacks) {
   onFriendLeftCallback = callbacks.friendLeft;
   onFriendConnectedCallback = callbacks.friendConnected;
   onDataChannelMessageCallback = callbacks.dataChannelMessage;
-  socket.emit('join', {roomId, name}, function(result){
+  socket.emit("join", { roomId, name }, function(result) {
     friends = result;
-    console.log('Joins', friends);
-    friends.forEach((friend) => {
+    console.log("Joins", friends);
+    friends.forEach(friend => {
       createPeerConnection(friend, true);
     });
-    if(callbacks.joined != null) {
+    if (callbacks.joined != null) {
       me = {
         socketId: socket.id,
         name: name
-      }
+      };
       callbacks.joined();
     }
   });
 }
 //------------------------------------------------------------------------------
 // Exports
-if(!IS_BROWSER) {
+if (!IS_BROWSER) {
   module.exports = {
     join,
     countFriends,
     getLocalStream,
     broadcastMessage
-  }
+  };
 }
